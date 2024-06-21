56bb235c4213d36745cc0c70461d755f4db2c7fa
seafairy <yousei1739@icloud.com>
2024-06-22 03:06:54 +0900

    基本的な機能を追加しました

---
+++ Assets/Title.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    private bool firstPush = false;
+
+    public void PressStart()
+    {
+        Debug.Log("Press Start!");
+        if (!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            SceneManager.LoadScene("Stage1");
+            firstPush = true;
+        }
+    }
+
+    public void PressStart2()
+    {
+        Debug.Log("Press Start!");
+        if (!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            SceneManager.LoadScene("Stage2");
+            firstPush = true;
+        }
+    }
+
+    public void PressStart3()
+    {
+        Debug.Log("Press Start!");
+        if (!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            SceneManager.LoadScene("Stage3");
+            firstPush = true;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
